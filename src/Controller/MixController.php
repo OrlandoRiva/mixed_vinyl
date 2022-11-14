61664b33de6f50d9eab07333453e377ba06e803e
OrlandoRiva <orlando.carvalho@riva-office.pt>
2022-11-14 12:33:06 +0000

    Finished Course 3

---
+++ src/Controller/MixController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MixController extends AbstractController
+{
+//    #[Route('/mix/new')]
+//    public function new(EntityManagerInterface $entityManager): Response
+//    {
+//        $mix = new VinylMix();
+//        $mix->setTitle('Do you Remember... Phil Collins?!');
+//        $mix->setDescription('A pure mix of drummers turned singers!');
+//        $genres = ['pop', 'rock'];
+//        $mix->setGenre($genres[array_rand($genres)]);
+//        $mix->setTrackCount(rand(5, 20));
+//        $mix->setVotes(rand(-50, 50));
+//
+//        $entityManager->persist($mix);
+//        $entityManager->flush();
+//
+//        return new Response(sprintf(
+//            'Mix %d is %d tracks of pure 80\'s heaven',
+//            $mix->getId(),
+//            $mix->getTrackCount()
+//        ));
+//    }
+
+    #[Route('/mix/{slug}', name: 'app_mix_show')]
+    public function show($slug, VinylMixRepository $mixRepository): Response
+    {
+        $mix = $mixRepository->findOneBy(['slug' => $slug]);
+
+        if (!$mix) {
+            throw $this->createNotFoundException('Mix not found');
+        }
+
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix
+        ]);
+    }
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote($id, VinylMixRepository $mixRepository, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $mix = $mixRepository->find($id);
+
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->upVote();
+        } else {
+            $mix->downVote();
+        }
+
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Vote counted!');
+
+        return $this->redirectToRoute('app_mix_show', [
+            'slug' => $mix->getSlug()
+        ]);
+    }
+
+//    Sensio feature
+//    #[Route('/mix/{id}', name: 'app_mix_show')]
+//    public function show(VinylMix $mix): Response
+//    {
+//        return $this->render('mix/show.html.twig', [
+//            'mix' => $mix
+//        ]);
+//    }
+
+}
